043aaf98349e001fdf88a97e9bfcd1956dd833cf
Peter Gazdik <peter.gazdik@gmail.com>
2025-01-06 10:06:37 +0100

    plausible-test

--- analytics-demo/src/pages/Pricing.tsx
+++ analytics-demo/src/pages/Pricing.tsx
@@ -3,15 +3,17 @@ import Card from "../components/Card";
 export default function Pricing() {
     return (
         <div>
-            <Card header="Home">
-                Once there was a magical elf who lived in a rainbow tree.<br />
-                He lived downstairs from a flatulent dwarf who was constantly having to pee.<br />
-                One day, the elf could take no more<br />
-                so he went to bang on the rude dwarf's door<br />
-                And what do you know, they suddenly both were married.
+            <Card header="Pricing">
+                With bony hands I hold my partner<br />
+                on soulless feet we cross the floor<br />
+                the music stops as if to answer<br />
+                an empty knocking at the door.<br />
+                <br />
+                it seems his skin was sweet as mango<br />
+                when last I held him to my breast<br />
+                but now, we dance this grim fandango<br />
+                and will for years until we rest.
             </Card>
         </div>
     )
-
-
 }
--- analytics-demo/vite.config.ts
+++ analytics-demo/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
+const isProduction = process.env.NODE_ENV === "production";
+//   base: isProduction ? "/analytics/" : "/",
+
 export default defineConfig({
   plugins: [solidPlugin()],
   server: {
